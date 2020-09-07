5bb391fdf11e722e8e9c6ff7c9f4cc102fdd887b
KaushalB <bommek@uw.edu>
2020-09-06 22:19:58 -0500

    storyboard framework

---
+++ SecApp/SecApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SecApp
+//
+//  Created by Kaushal Bommena on 2020/9/6.
+//  Copyright © 2020 Kaushal Bommena. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
