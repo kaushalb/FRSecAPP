d1684d73c2285fd8a82873b7757a546cd48ff40d
KaushalB <bommek@uw.edu>
2020-09-06 21:26:28 -0500

    attempting swift

---
+++ secapp.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+// This is a trial Xcode file for swift
+// TO DO:
+// - Understand Swift documentation
+// - Use file as playground
+// - Execute file and display results.
+
+var str = "Hello, playground"
+print(str)
+
